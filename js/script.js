d6b8e7da7a48f8ac05908170715dc191375e3764
yarynayavor <yourhurt09@gmail.com>
2017-08-31 10:26:42 +0300

    Added index.html with external working script

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var personName = prompt("Enter name:");
+var time = prompt("Enter time in true format as example shows:","00:00");
+var place = prompt("Enter place:");
+var event = prompt("Enter event:");;
+
+
+console.log(personName + " " +"has a"+ " "+ event + " "+ "today at"+ " "+time+ " "+"somewhere in" +" "+place+".");
