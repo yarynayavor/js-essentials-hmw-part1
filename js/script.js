0c73840af095dd0ff3eacdd028c583d986878902
Yaryna Yavor <yourhurt09@gmail.com>
2017-09-01 13:04:08 +0300

    remove another ' ; '

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var personName = prompt("Enter name:","Petro");
 var time = prompt("Enter time in true format as example shows:","00:00");
 var place = prompt("Enter place:","Ukraine");
-var event = prompt("Enter event:","meeting");;
+var event = prompt("Enter event:","meeting");
 
 
 console.log(personName + " " +"has a"+ " "+ event + " "+ "today at"+ " "+time+ " "+"somewhere in" +" "+place+".");
