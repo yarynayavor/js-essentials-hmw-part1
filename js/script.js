6b47e3eed2da175feb713fb0d108274fb6a9d187
Yaryna Yavor <yourhurt09@gmail.com>
2017-09-03 00:46:55 +0300

    added  some validator when entering text

--- js/script.js
+++ js/script.js
@@ -1,7 +1,47 @@
-var personName = prompt("Enter name:","Petro");
-var time = prompt("Enter time in true format as example shows:","00:00");
-var place = prompt("Enter place:","Ukraine");
-var event = prompt("Enter event:","meeting");
+function name() {
+	var personName = prompt("Enter name:","Petro");
+	if(personName==='') {
+		alert("Name can't be empty!");
+		personName = prompt("Enter name:","Petro");
+	}
+	return personName;
+}
 
+personName=name();
 
-console.log(personName + " " +"has a"+ " "+ event + " "+ "today at"+ " "+time+ " "+"somewhere in" +" "+place+".");
+function time() {
+	var sometime = prompt("Enter time in true format as example shows:","00:00");
+	if(sometime==='') {
+		alert("Time can't be empty and please write in true format 00:00 !");
+		sometime = prompt("Enter time in true format as example shows:","00:00");
+	}
+	return sometime;
+}
+
+sometime=time();
+
+function place() {
+	var somePlace = prompt("Enter place:","Ukraine");
+	if(somePlace==='') {
+		alert("Whoa..Whoa..Please write a place not empty!");
+		somePlace = prompt("Enter place:","Ukraine");
+	}
+	return somePlace;
+}
+
+somePlace=place();
+
+function eventTest() {
+	var event = prompt("Enter event:","meeting");
+	if(event==='') {
+		alert("Event can't be empty. Please write some event below!");
+		event = prompt("Enter event:","meeting");
+	}
+	return event;
+}
+
+event=eventTest();
+
+alert("Go to console!!!");
+
+console.log(personName + " " +"has a"+ " "+ event + " "+ "today at"+ " "+sometime+ " "+"somewhere in" +" "+somePlace+".");
